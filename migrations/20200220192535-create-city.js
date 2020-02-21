b4ad783a7e51b5791eb55aa72ad1e9b7e23258db
John William Davis <jwdavisdeveloper@gmail.com>
2020-02-21 08:55:08 -0800

    tried to fix broken pages, using sarah repo

---
+++ migrations/20200220192535-create-city.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      lat: {
+        type: Sequelize.NUMERIC,
+        allowNull: false
+      },
+      long: {
+        type: Sequelize.NUMERIC,
+        alllowNull: false
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('cities');
+  }
+};
--- routes/cities.js
+++ routes/cities.js
@@ -1,42 +1,60 @@
-require('dotenv').config();
-
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const db = require('../models');
+
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const geocodingClient = mbxGeocoding({accessToken: process.env.MAPBOX_API});
+const geocodeingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_API_KEY });
 
-router.get('/results', (req, res) => {
-    geocodingClient.forwardGeocode({query: req.query.name + ' ' + req.query.state})
-    .send().then(response=> {
-        let places = response.body.features.filter(result=> {
-            if (result['place_type'][0]=== 'place') {
-                return true
-            }
-    })
-})
-
-router.get("/test", (req, res) => {
-  geocodingClient
-    .forwardGeocode({
-      query: "Seattle, WA"
-    })
-    .send()
-    .then(response=> {
-        //send an array of only places
-        let places = response.body.features.filter(result=> {
-            if (result['place_type'][0]=== 'place') {
-                return true
-            }
-        }).map(city=>{
-            return {
-                name: city['place_name'].split(', ')[0],
-                state: city['place_name'].split(', ')[1],
-                lat: city.center[1],
-                long: city.center[0]
-            }
-        });
-        res.send(places);
+router.get('/results', (req, res)=>{
+  geocodeingClient.forwardGeocode({ query: req.query.name + ' ' + req.query.state })
+  .send().then(response=>{
+    let places = response.body.features.filter(result=>{
+      if (result['place_type'][0] === 'place') {
+        return true
+      }
+    }).map(city=>{
+      return {
+        name: city['place_name'].split(', ')[0],
+        state: city['place_name'].split(', ')[1],
+        lat: city.center[1],
+        long: city.center[0]
+      }
     });
+    res.render('results', { results: places });
+  });
+});
+
+router.post('/faves', (req, res)=>{
+  console.log("Add fave to db");
+  console.log(req.body);
+  db.city.findOrCreate({
+    where: {
+      lat: req.body.lat,
+      long: req.body.long
+    }, defaults: {
+      name: req.body.name,
+      state: req.body.state
+    }
+  }).then(([city, created])=>{
+    console.log(`City ${city.name} was ${created ? 'created' : 'found'}`);
+    res.redirect('/faves');
+  }).catch(err=>{
+    console.log('💩 error in creating fave in DB')
+    console.log(err);
+    res.render('404');
+  });
+});
+
+router.get('/faves', (req, res)=>{
+  // get all faves from db
+  db.city.findAll().then(cities=>{
+    // send all faves to front end
+    res.render('faves', { cities });
+  }).catch(err=>{
+    console.log('💩 error in finding all faves in DB')
+    console.log(err);
+    res.render('404');
+  });
 });
 
 module.exports = router;
